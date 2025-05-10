d43a28d53e07f10787d3e6c6a921047843ab7f9a
Ⴆყ ᴴᴬᵀ <by.hat@outlook.com>
2025-05-10 03:26:17 +0300

    Update save_ip.php

--- save_ip.php
+++ save_ip.php
@@ -1,10 +1,17 @@
-<?php
-// Dosya yazma testi
-$file = 'log.txt';
-$testData = "Test data at " . date('Y-m-d H:i:s') . "\n";
-if (file_put_contents($file, $testData, FILE_APPEND)) {
-    echo "Veri başarıyla yazıldı.";
+if (isset($_POST['ip'])) {
+    // IP adresini alıyoruz
+    $userIP = $_POST['ip'];
+    
+    // Log dosyasının yolu
+    $file = 'log.txt';
+    
+    // IP adresi ve zaman bilgisini formatlıyoruz
+    $currentDateTime = date('Y-m-d H:i:s'); // Zaman damgası
+    $logData = "IP: $userIP - Zaman: $currentDateTime\n";
+    
+    // Dosyaya yazma işlemi (append modunda)
+    file_put_contents($file, $logData, FILE_APPEND);
+    echo 'IP başarıyla kaydedildi';
 } else {
-    echo "Veri yazılamadı.";
+    echo 'IP adresi bulunamadı';
 }
-?>
